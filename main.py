df2573c52d2419dd3a3806bda2170cd5ecb64b3e
PatientAssess <mister.mol4anov-5@yandex.ru>
2025-07-31 12:12:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -801,8 +801,8 @@ def get_events(token:token1):
 	    for req in requests:
 	    	date = req.get("date")
 	    	time = req.get("time")
-		req_id = req.get("req_id")
 	    	doc = get_doc_with_id(appointment[1])
+		req_id = req.get("req_id")
 	    	new = {"start": f'{date}T{time}:00', "end": f'{date}T{time[0]}{time[1]}:30:00',"doc_name": doc.get('name'),"doc_surname": doc.get('surname'),"father_name": doc.get('father_name'),"doc_specialty": doc.get('specialty'), "email": doc.get('email'), "req_id":{req_id}}
 	    	returned.append(new)
 	
@@ -818,8 +818,8 @@ def get_events(token:token1):
 	    for req in requests:
 	    	date = req.get("date")
 	    	time = req.get("time")
-		req_id = req.get("req_id")
 	    	doc = get_doc_with_id(appointment[1])
+		req_id = req.get("req_id")
 	    	new = {"start": f'{date}T{time}:00', "doc_name": doc.get('name'),"doc_surname": doc.get('surname'),"father_name": doc.get('father_name'),"doc_specialty": doc.get('specialty'), "email": doc.get('email') , "req_id":{req_id}}
 	    	returned.append(new)
 	
