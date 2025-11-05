448e102d2fc84b9d9354d7b341167618ff4490df
PatientAssess <mister.mol4anov-5@yandex.ru>
2025-11-05 04:35:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -346,7 +346,7 @@ def token_response(token: str):
 def signJWT(user_id: str) -> Dict[str, str]:
     payload = {
         "user_id": user_id,
-        "expires": time.time() + 3000
+        "expires": time.time() + 30000
     }
     token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
 
